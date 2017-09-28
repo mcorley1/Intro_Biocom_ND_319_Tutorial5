97410a6b49a99ae4fc5512fe043a88ef698f2589
kkilgoreND <31992787+kkilgoreND@users.noreply.github.com>
2017-09-28 10:21:52 -0400

    Updated Exercise_5_Part_3.py

--- Exercise_5_Part_3.py
+++ Exercise_5_Part_3.py
@@ -1,17 +1,19 @@
 import pandas
-pandas.read_csv('wages.csv')
+#Define wages
+wages=pandas.read_csv('wages.csv')
 
 #Select for the people who didn't finish school(i.e. 12yrs of school)
-education12=wages[wages.yearSchool==12]
+education12=wages[wages.yearsSchool==12]
 
 #Calculate the minimum wage for people with 12yrs of school
 minimum12=min(education12.wage)
 
 #Select for the people who did finish school(i.e. 16yrs of school)
-education16=wages[wages.yearSchool==16]
+education16=wages[wages.yearsSchool==16]
 
 #Calculate the minimum wage for people with 16yrs of school
 minimum16=min(education16.wage)
 
 #Calculate the difference between the minimum wage for those who finished school vs. those who didn't
 print(minimum16-minimum12)
+#Difference is 4.0816223772
